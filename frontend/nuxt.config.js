574c7a5238d9e85b46a3acf462c078bf209c624d
Nikolai <nikolaichub.me@gmail.com>
2022-11-21 23:31:50 +0200

    feat: googlefonts setup, main.css

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,3 +1,11 @@
 import { defineNuxtConfig } from 'nuxt/config';
 
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+  css: ['~/assets/css/main.css'],
+  buildModules: ['@nuxtjs/google-fonts'],
+  googleFonts: {
+    families: {
+      Inter: [400, 500, 600],
+    },
+  },
+});
