9927eb50e5448639b4c32e6aa6a74d6dd220e1fa
Nikolai <nikolaichub.me@gmail.com>
2022-12-25 20:37:21 +0200

    feat: only authorized users can have access

---
+++ frontend/middleware/auth.global.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import { defineNuxtRouteMiddleware, useCookie, navigateTo } from '#imports';
+
+export default defineNuxtRouteMiddleware((to) => {
+  const { path } = to;
+  if (path === '/' || path === '/auth/google') return;
+
+  // only authorized users
+  const tokenCookie = useCookie('token');
+  if (!tokenCookie.value) {
+    return navigateTo('/');
+  }
+});
