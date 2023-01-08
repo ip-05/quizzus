3263d7a55ab5a83cc5c6f089d3ffe3d4e8d8601d
Artem <bondarchuk.artem20@gmail.com>
2023-01-08 14:09:00 +0200

    fix: test config

--- backend/tests/config_test.go
+++ backend/tests/config_test.go
@@ -1,42 +1,48 @@
 package tests
 
 import (
+	"testing"
+
 	"github.com/ip-05/quizzus/config"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
-func TestConfigServer(t *testing.T) {
+func TestInitConfig(t *testing.T) {
 	cfg := config.InitConfig("test")
 
-	assert.Equal(t, false, cfg.Server.Secure, "should be equal")
-	assert.Equal(t, "localhost", cfg.Server.Domain, "should be equal")
-	assert.Equal(t, "http://localhost:1234", cfg.Server.Base, "should be equal")
-	assert.Equal(t, "localhost", cfg.Server.Host, "should be equal")
-	assert.Equal(t, int64(1234), cfg.Server.Port, "should be equal")
-}
+	t.Run("TestConfigServer", func(t *testing.T) {
+		assert.Equal(t, false, cfg.Server.Secure, "should be equal")
+		assert.Equal(t, "localhost", cfg.Server.Domain, "should be equal")
+		assert.Equal(t, "http://localhost:1234", cfg.Server.Base, "should be equal")
+		assert.Equal(t, "localhost", cfg.Server.Host, "should be equal")
+		assert.Equal(t, int64(1234), cfg.Server.Port, "should be equal")
+	})
 
-func TestConfigGoogle(t *testing.T) {
-	cfg := config.InitConfig("test")
+	t.Run("TestConfigGoogle", func(t *testing.T) {
+		assert.Equal(t, "id", cfg.Google.ClientId, "should be equal")
+		assert.Equal(t, "secret", cfg.Google.ClientSecret, "should be equal")
+	})
 
-	assert.Equal(t, "id", cfg.Google.ClientId, "should be equal")
-	assert.Equal(t, "secret", cfg.Google.ClientSecret, "should be equal")
-}
+	t.Run("TestConfigSecrets", func(t *testing.T) {
+		assert.Equal(t, "jwt", cfg.Secrets.Jwt, "should be equal")
+	})
 
-func TestConfigSecrets(t *testing.T) {
-	cfg := config.InitConfig("test")
+	t.Run("TestConfigFrontend", func(t *testing.T) {
+		assert.Equal(t, "http://localhost:4321", cfg.Frontend.Base, "should be equal")
+	})
 
-	assert.Equal(t, "jwt", cfg.Secrets.Jwt, "should be equal")
-}
+	t.Run("TestConfigDB", func(t *testing.T) {
+		assert.Equal(t, "localhost", cfg.Database.Host, "should be equal")
+		assert.Equal(t, int64(5432), cfg.Database.Port, "should be equal")
+		assert.Equal(t, "user", cfg.Database.User, "should be equal")
+		assert.Equal(t, "password", cfg.Database.Password, "should be equal")
+		assert.Equal(t, "name", cfg.Database.DbName, "should be equal")
+		assert.Equal(t, false, cfg.Database.Secure, "should be equal")
+	})
 
-func TestConfigFrontend(t *testing.T) {
-	cfg := config.InitConfig("test")
-
-	assert.Equal(t, "http://localhost:4321", cfg.Frontend.Base, "should be equal")
-}
-
-func TestConfigInvalid(t *testing.T) {
-	assert.Panics(t, func() {
-		config.InitConfig("test_panic")
-	}, "should panic")
+	t.Run("TestConfigInvalid", func(t *testing.T) {
+		assert.Panics(t, func() {
+			config.InitConfig("test_panic")
+		}, "should panic")
+	})
 }
