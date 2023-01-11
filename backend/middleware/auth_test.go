351df1660dc32532f153bf059b9c669c2f3c5dcf
JenyaFTW <yyakovliev02@gmail.com>
2023-01-11 10:30:02 +0200

    feat: convert auth middleware into test suite

--- backend/middleware/auth_test.go
+++ backend/middleware/auth_test.go
@@ -4,13 +4,21 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/ip-05/quizzus/config"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestAuthMiddleware(t *testing.T) {
+type AuthMiddlewareSuite struct {
+	suite.Suite
+	ctx    *gin.Context
+	engine *gin.Engine
+	w      *httptest.ResponseRecorder
+}
+
+func (s *AuthMiddlewareSuite) SetupTest() {
 	// Given
 	cfg := &config.Config{
 		Secrets: &config.SecretConfig{
@@ -18,39 +26,45 @@ func TestAuthMiddleware(t *testing.T) {
 		},
 	}
 
+	authMiddleware := AuthMiddleware(cfg)
+
 	gin.SetMode(gin.TestMode)
+	s.w = httptest.NewRecorder()
 
-	w := httptest.NewRecorder()
-	ctx, engine := gin.CreateTestContext(w)
-
-	engine.Use(AuthMiddleware(cfg))
+	ctx, engine := gin.CreateTestContext(s.w)
+	engine.Use(authMiddleware)
 	engine.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "OK")
 	})
 
-	t.Run("should return forbidden", func(t *testing.T) {
-		// When
-		ctx.Request, _ = http.NewRequest(http.MethodGet, "/", nil)
-		engine.ServeHTTP(w, ctx.Request)
+	s.ctx = ctx
+	s.engine = engine
+}
 
-		// Then
-		assert.Equal(t, http.StatusForbidden, w.Code)
-	})
+func (s *AuthMiddlewareSuite) Test_Forbidden() {
+	// When
+	s.ctx.Request, _ = http.NewRequest(http.MethodGet, "/", nil)
+	s.engine.ServeHTTP(s.w, s.ctx.Request)
 
-	t.Run("should pass through", func(t *testing.T) {
-		// When
-		w = httptest.NewRecorder()
+	// Then
+	assert.Equal(s.T(), http.StatusForbidden, s.w.Code)
+}
 
-		ctx.Request, _ = http.NewRequest(http.MethodGet, "/", nil)
-		ctx.Request.Header.
-			Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpvaG5AZG9lLmNvbSIsImV4cCI6MTY3Mzk5MDM1OCwiaWQiOiIxMjMxMjMiLCJuYW1lIjoiSm9obiIsInByb2ZpbGVQaWN0dXJlIjoiaHR0cHM6Ly9qb2huLmRvZS5jb20vcGljdHVyZS5wbmcifQ.J2Vx9KoqptH1jFvbXEP-VrPngSC4TfgYAvsj7DVy_J4")
-		engine.ServeHTTP(w, ctx.Request)
+func (s *AuthMiddlewareSuite) Test_OK() {
+	// When
+	s.ctx.Request, _ = http.NewRequest(http.MethodGet, "/", nil)
+	s.ctx.Request.Header.
+		Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpvaG5AZG9lLmNvbSIsImV4cCI6MTY3Mzk5MDM1OCwiaWQiOiIxMjMxMjMiLCJuYW1lIjoiSm9obiIsInByb2ZpbGVQaWN0dXJlIjoiaHR0cHM6Ly9qb2huLmRvZS5jb20vcGljdHVyZS5wbmcifQ.J2Vx9KoqptH1jFvbXEP-VrPngSC4TfgYAvsj7DVy_J4")
+	s.engine.ServeHTTP(s.w, s.ctx.Request)
 
-		// Then
-		body, err := io.ReadAll(w.Body)
-		assert.Nil(t, err)
+	// Then
+	body, err := io.ReadAll(s.w.Body)
+	assert.Nil(s.T(), err)
 
-		assert.Equal(t, http.StatusOK, w.Code)
-		assert.Equal(t, "OK", string(body))
-	})
+	assert.Equal(s.T(), http.StatusOK, s.w.Code)
+	assert.Equal(s.T(), "OK", string(body))
+}
+
+func TestAuthMiddleware(t *testing.T) {
+	suite.Run(t, new(AuthMiddlewareSuite))
 }
