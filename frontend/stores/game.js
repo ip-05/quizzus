fc9c5716a414d6be334c1e5f5d88875fd9245838
Nikolai <nikolaichub.me@gmail.com>
2023-01-12 02:01:38 +0200

    feat: game store with websocket logic

--- frontend/stores/game.js
+++ frontend/stores/game.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import { defineStore } from 'pinia';
-import { ref, reactive, computed } from 'vue';
+import { ref, reactive, computed, onMounted, watch } from 'vue';
 import { useErrorStore } from './error';
-import { useRuntimeConfig, useCookie } from '#imports';
+import { useAuthStore } from './auth';
+import { useSocketStore } from './socket';
+import { useRuntimeConfig, useCookie, navigateTo, useRoute } from '#imports';
 
 /**
  * game-wait
@@ -20,25 +22,50 @@ export const useGameStore = defineStore('GameStore', () => {
   const errorStore = useErrorStore();
   const tokenCookie = useCookie('token');
 
+  const authStore = useAuthStore();
+
+  let socketStore = null;
+  onMounted(() => {
+    const { path } = useRoute();
+    if (path.startsWith('/game/') || path.startsWith('/console/')) {
+      console.log('trying to connect');
+      socketStore = useSocketStore();
+    }
+  });
+
   // Game Mode
-  const state = ref('game-graph-admin');
+  const state = ref('game-wait');
   const active = ref(false);
 
   // Game Info
   const gameId = ref(0);
   const inviteCode = ref('');
+  const owner = ref(null);
+  const isOwner = computed(() => authStore.user.id === owner.value);
 
   const topic = ref('' || 'Game topic');
   const totalPoints = ref(0);
   const points = ref(null || 10);
   const roundTime = ref(null || 20);
+  const timer = ref(null); // timer for dynamic island for all users synchronously
   const participants = ref([]);
   const participantsNumber = computed(() => participants.value.length);
 
-  const id = ref(0); // Local questions id for Vue to render properly
+  const gameStarted = ref(false);
+  const countdown = ref(null);
+  const canContinue = ref(true); // can admin start game or give another question (depens on game state)
+
+  const id = ref(63457378356); // Local questions id for Vue to render properly
   const questions = ref([]);
-  appendQuestion();
-  const questionsNumber = computed(() => questions.value.length);
+  // appendQuestion();
+  const questionsNumber = computed({
+    get() {
+      return questions.value.length;
+    },
+    set(count) {
+      questions.value = new Array(count);
+    },
+  });
 
   function appendQuestion() {
     id.value += 1;
@@ -127,7 +154,13 @@ export const useGameStore = defineStore('GameStore', () => {
         },
         params: query,
       });
+      // for regular user to join
+      if (data.message === 'Game found' && data.topic) {
+        inviteCode.value = query.invite_code;
+        return 'player';
+      }
       fillGameFields(data);
+      return 'admin';
     } catch (error) {
       const message = error.response.data.error;
       errorStore.message = message;
@@ -166,6 +199,8 @@ export const useGameStore = defineStore('GameStore', () => {
     points.value = data.points;
     roundTime.value = data.roundTime;
     questions.value = data.questions;
+    owner.value = data.ownerId;
+    console.log(questions.value);
   }
 
   // User exits and game resets
@@ -181,6 +216,7 @@ export const useGameStore = defineStore('GameStore', () => {
   const currentQuestion = reactive({
     name: 'Question name',
     id: 0,
+    answer: null,
     options: [
       {
         name: 'Option A',
@@ -210,12 +246,28 @@ export const useGameStore = defineStore('GameStore', () => {
     id: 2,
   });
 
-  const submittedAnswers = ref([
-    { id: 0, answers: 8 },
-    { id: 1, answers: 6 },
-    { id: 2, answers: 15 },
-    { id: 3, answers: 0 },
-  ]);
+  const selectedOption = ref(null);
+  const submittedAnswersByUsers = ref({});
+  /**
+   * userId: idx
+   * userId2: idx2
+   */
+
+  const submittedAnswers = computed(() => {
+    const answers = [
+      { id: 0, answers: 0 },
+      { id: 1, answers: 0 },
+      { id: 2, answers: 0 },
+      { id: 3, answers: 0 },
+    ];
+
+    for (const userId of Object.keys(submittedAnswersByUsers.value)) {
+      const option = submittedAnswersByUsers.value[userId];
+      answers[option].answers += 1;
+    }
+
+    return answers;
+  });
 
   const graphAnswers = computed(() => {
     const answers = submittedAnswers.value.map(({ answers }) => answers);
@@ -226,27 +278,19 @@ export const useGameStore = defineStore('GameStore', () => {
   });
 
   const leaderboardShown = ref(false);
-  const leaderboard = reactive([
-    {
-      points: 175,
-      participant: 'Joe Biden',
-    },
-    {
-      points: 150,
-      participant: 'Sussy baka',
-    },
-    {
-      points: 200,
-      participant: 'Amogus',
-    },
-    {
-      points: 30,
-      participant: 'zaza',
-    },
-  ]);
-  const leaderboardSorted = computed(() => leaderboard.sort((a, b) => b.points - a.points));
-  const top3 = computed(() => leaderboardSorted.value.slice(0, 3));
-  const winner = computed(() => leaderboardSorted.value[0]);
+  const leaderboard = reactive([]);
+  const leaderboardSorted = computed(() =>
+    leaderboard.sort((a, b) => b.points - a.points || a.participant.localeCompare(b.participant))
+  );
+  const top3 = computed(() => {
+    const { length } = leaderboardSorted.value;
+    if (!length) return false;
+    return leaderboardSorted.value.slice(0, 3);
+  });
+  const winner = computed(() => {
+    const { length } = leaderboardSorted.value;
+    return length ? leaderboardSorted.value[0] : false;
+  });
 
   // Hint in the bottom of the game display
   const hint = computed(() => {
@@ -274,7 +318,7 @@ export const useGameStore = defineStore('GameStore', () => {
     state.value = 'game-in';
   }
   function setGameOverClient() {
-    state.value = 'game-over-client';
+    state.value = 'game-over';
   }
   function setGameOverAdmin() {
     state.value = 'game-over-admin';
@@ -289,8 +333,156 @@ export const useGameStore = defineStore('GameStore', () => {
     state.value = 'game-graph-admin';
   }
 
-  function leaveGame() {
-    console.log('User left game');
+  /**
+   * WebSockets event
+   * ----------------
+   */
+
+  // user sends event when joins the game
+  function joinedGame(data) {
+    // for admin
+    if (isOwner.value) {
+      setGameWaitAdmin();
+      return;
+    }
+    active.value = true;
+    // for regular players
+    setGameWait();
+    points.value = data.points;
+    roundTime.value = data.roundTime;
+    topic.value = data.topic;
+    questionsNumber.value = data.questionCount;
+    const { owner, members } = data;
+    for (const member of Object.keys(members)) {
+      if (member !== owner.id) {
+        const { id, name, profilePicture } = members[member];
+        participants.value.push({ id, name, profilePicture });
+      }
+    }
+  }
+
+  // recieved when another player joins game
+  function userJoined(data) {
+    const { id, name, profilePicture } = data;
+    participants.value.push({ id, name, profilePicture });
+  }
+
+  // admin sends event
+  function startGame() {
+    active.value = true;
+    gameStarted.value = true;
+    canContinue.value = false;
+    socketStore.send({
+      message: 'START_GAME',
+    });
+  }
+
+  // player sends when leaves
+  function userLeft(data) {
+    participants.value = participants.value.filter(({ profilePicture }) => data.profilePicture !== profilePicture);
+  }
+
+  // admin starts game and countdown starts for every user
+  function gameStarting(data) {
+    countdown.value = data;
+  }
+
+  // event right after countdown ending
+  function gameInProgress() {
+    countdown.value = null;
+  }
+
+  function roundInprogress(data) {
+    if (data) {
+      currentQuestion.id = data.question.id;
+      currentQuestion.name = data.question.name;
+      currentQuestion.options = data.question.options;
+      currentQuestion.answer = null;
+      timer.value = data.timer;
+    }
+    // for admin displays graph of answers
+    if (isOwner.value) {
+      setGameGraphAdmin();
+      return;
+    }
+    // for players displays currect question
+    setGameIn();
+  }
+
+  function userAnswered(data) {
+    submittedAnswersByUsers.value[data.user] = data.option;
+  }
+
+  // player gets info about their aswers
+  function roundFinished(data) {
+    const answer = data.options.filter(({ correct }) => correct)[0];
+    currentQuestion.answer = answer.name;
+    currentQuestion.options = data.options;
+    correctAnswer.value = answer;
+    selectedOption.value = null;
+    totalPoints.value = data.leaderboard[authStore.user.id];
+    submittedAnswersByUsers.value = {};
+    // displaying default wait screen for admin
+    if (isOwner.value) {
+      setGameWaitAdmin();
+      canContinue.value = true;
+      return;
+    }
+    // displaying correct screen if player is correct
+    if (data.correct) {
+      setGameCorrect();
+      return;
+    }
+    // displaying correct screen if player is wrong
+    setGameWrong();
+  }
+
+  // admin gives another round
+  function nextQuestion() {
+    socketStore.send({
+      message: 'NEXT_ROUND',
+    });
+    canContinue.value = false;
+  }
+
+  // player submits answer
+  watch(selectedOption, (s) => {
+    answerQuestion(s);
+  });
+
+  function answerQuestion(data) {
+    socketStore.send({
+      message: 'ANSWER_QUESTION',
+      data: {
+        option: data,
+      },
+    });
+  }
+
+  function gameFinished(data) {
+    gameStarted.value = false;
+    canContinue.value = false;
+    Object.assign(
+      leaderboard,
+      participants.value.map(({ id, name }) => ({ id, participant: name, points: data[id] }))
+    );
+    if (isOwner.value) {
+      setGameOverAdmin();
+      return;
+    }
+    setGameOverClient();
+  }
+
+  // admin of the game left - all users go home
+  function hostLeft() {
+    active.value = false;
+    navigateTo('/');
+  }
+
+  // if users tries to get in before admin
+  function gameForbidden() {
+    active.value = false;
+    navigateTo('/');
   }
 
   return {
@@ -299,8 +491,10 @@ export const useGameStore = defineStore('GameStore', () => {
     topic,
     totalPoints,
     inviteCode,
+    countdown,
     points,
     roundTime,
+    timer,
     questions,
     questionsNumber,
     removeQuestion,
@@ -313,6 +507,7 @@ export const useGameStore = defineStore('GameStore', () => {
     leaderboard,
     leaderboardSorted,
     top3,
+    selectedOption,
     submittedAnswers,
     graphAnswers,
     winner,
@@ -330,6 +525,23 @@ export const useGameStore = defineStore('GameStore', () => {
     setGameCorrect,
     setGameWrong,
     setGameGraphAdmin,
-    leaveGame,
+    isOwner,
+    //
+    gameStarted,
+    canContinue,
+    joinedGame,
+    userJoined,
+    userLeft,
+    startGame,
+    gameStarting,
+    gameInProgress,
+    roundInprogress,
+    roundFinished,
+    answerQuestion,
+    userAnswered,
+    hostLeft,
+    gameForbidden,
+    nextQuestion,
+    gameFinished,
   };
 });
