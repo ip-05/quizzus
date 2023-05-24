9ff0074d55dc34d94ecbdfd93c1144dcd4c6c826
Artem <bondarchuk.artem20@gmail.com>
2023-05-24 12:44:51 +0300

    feat: moved game service interface to web, changed it's name

--- backend/app/game/interface.go
+++ backend/app/game/interface.go
@@ -2,17 +2,10 @@ package game
 
 import "github.com/ip-05/quizzus/entity"
 
-type IGame interface {
+type IGameRepo interface {
 	Get(id int, code string) (*entity.Game, error)
 	Create(e *entity.Game) *entity.Game
 	Update(id int, code string, e *entity.Game) (*entity.Game, error)
 	Delete(id int, code, userId string) error
 	DeleteQuestion(id int)
-}
-
-type IService interface {
-	CreateGame(body entity.CreateBody, ownerId string) (*entity.Game, error)
-	UpdateGame(body entity.UpdateBody, id int, code, ownerId string) (*entity.Game, error)
-	DeleteGame(id int, code, userId string) error
-	GetGame(id int, code string) (*entity.Game, error)
 }
