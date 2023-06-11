3392125619287e2d7ac391887278e7b83c64943d
JenyaFTW <yyakovliev02@gmail.com>
2023-06-11 19:53:33 +0300

    feat: google, discord, telegram entities

--- backend/entity/user.go
+++ backend/entity/user.go
@@ -1,9 +1,12 @@
 package entity
 
+import "fmt"
+
 type User struct {
 	Id          string   `json:"id"`
 	GoogleId    string   `json:"google_id"`
 	DiscordId   string   `json:"discord_id"`
+	TelegramId  string   `json:"telegram_id"`
 	Picture     string   `json:"picture"`
 	Name        string   `json:"name"`
 	SavedGames  []string `json:"saved_games"`
@@ -19,17 +22,40 @@ type GoogleUser struct {
 }
 
 type DiscordUser struct {
+	Id       string `json:"id"`
+	Username string `json:"username"`
+	Avatar   string `json:"avatar"`
 }
 
 type TelegramUser struct {
+	Id       string `json:"id"`
+	Username string `json:"username"`
+	PhotoUrl string `json:"photo_url"`
 }
 
 func NewGoogleUser(body GoogleUser) (*User, error) {
 	user := &User{
-		GoogleId:  body.Id,
-		DiscordId: "",
-		Picture:   body.Picture,
-		Name:      body.GivenName,
+		GoogleId: body.Id,
+		Picture:  body.Picture,
+		Name:     body.GivenName,
+	}
+	return user, nil
+}
+
+func NewDiscordUser(body DiscordUser) (*User, error) {
+	user := &User{
+		DiscordId: body.Id,
+		Picture:   fmt.Sprintf("https://cdn.discordapp.com/avatars/%s/%s.png", body.Id, body.Avatar),
+		Name:      body.Username,
+	}
+	return user, nil
+}
+
+func NewTelegramUser(body TelegramUser) (*User, error) {
+	user := &User{
+		TelegramId: body.Id,
+		Picture:    body.PhotoUrl,
+		Name:       body.Username,
 	}
 	return user, nil
 }
