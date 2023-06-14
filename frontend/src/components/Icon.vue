b8262ccc0d98aa45ce67b035227ba83f986759ed
Nikolai <nikolaichub.me@gmail.com>
2023-06-14 19:42:15 +0300

    feat: Icon components for dynamic icon render

---
+++ frontend/src/components/Icon.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  icon: {
+    type: String,
+    default: 'icon-plus.svg',
+    required: true,
+  },
+});
+
+const svgs = {
+  plus: 'icon-plus.svg',
+};
+
+const getImageUrl = (name) => new URL(`../assets/svg/${name}`, import.meta.url).href;
+
+const source = computed(() => getImageUrl(svgs[props.icon] ?? props.icon));
+</script>
+
+<template>
+  <img :src="source" alt="Icon" />
+</template>
