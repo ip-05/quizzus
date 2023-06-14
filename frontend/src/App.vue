46d4c5239d03d9601702fb155e5d489f84adab43
Nikolai <nikolaichub.me@gmail.com>
2023-06-14 13:29:37 +0300

    feat: editorconfig, prettier: semi true, trailingComma always

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
