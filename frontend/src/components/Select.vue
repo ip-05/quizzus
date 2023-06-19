0911dcf1d7d080b6a38cf9f03a048e7c849f06dc
Nikolai <nikolaichub.me@gmail.com>
2023-06-20 01:45:15 +0300

    feat: select component, styled v-select library

---
+++ frontend/src/components/Select.vue
@@ -0,0 +1,36 @@
+<script setup>
+/**
+ *
+ * This is a wrapper component for v-select
+ * https://github.com/sagalbot/vue-select
+ *
+ */
+
+import { defineProps, defineEmits, ref, watch } from 'vue';
+
+const emit = defineEmits(['update:modelValue']);
+defineProps({
+  placeholder: {
+    type: String,
+    default: 'Please select one',
+  },
+  options: {
+    type: Array,
+  },
+  modelValue: {
+    type: [String, Number],
+    default: null,
+  },
+});
+
+const selected = ref('');
+watch(selected, (value) => emit('update:modelValue', value));
+</script>
+
+<template>
+  <div class="form">
+    <v-select v-model="selected" :options="options" class="select"></v-select>
+  </div>
+</template>
+
+<style scoped></style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,9 +5,11 @@ import { createPinia } from 'pinia';
 
 import App from './App.vue';
 import router from './router';
+import VueSelect from 'vue-select';
 
 const app = createApp(App);
 
+app.component('v-select', VueSelect);
 app.use(createPinia());
 app.use(router);
 
